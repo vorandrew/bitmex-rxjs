06004f61ca64aeae671d9ec79fd50bdfdae4e42e
Andrey Vorobyov <vorandrew@gmail.com>
2019-03-16 23:35:44 +0200

    Debug + no share

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ import 'dotenv/config'
 import crypto from 'crypto'
 import got from 'got'
 import qs from 'querystring'
+import Debug from 'debug'
 
 import { Subject, from, concat, timer } from 'rxjs'
 
-import { distinctUntilChanged, share, switchMap } from 'rxjs/operators'
+import { distinctUntilChanged, share, tap, switchMap } from 'rxjs/operators'
 
 import BitMEXClient from 'bitmex-realtime-api'
 
@@ -14,6 +15,11 @@ const client = new BitMEXClient({
   apiKeyID: process.env.BITMEX_API_KEY || null,
   apiKeySecret: process.env.BITMEX_API_SECRET || null,
 })
+
+const debugOrders = Debug('bitmex-rxjs:orders')
+const debugOrderEvents = Debug('bitmex-rxjs:order_events')
+const debugPositions = Debug('bitmex-rxjs:position')
+const debugPrice = Debug('bitmex-rxjs:price')
 
 const priceTmp = new Subject()
 const positionTmp = new Subject()
@@ -36,17 +42,22 @@ client.addStream('XBTUSD', 'position', data => {
 
 const orders$ = concat(timer(0), orderEvents).pipe(
   switchMap(() => from(ordersPromise())),
+  tap(debugOrders),
+)
+
+const orderEvents$ = orderEvents.pipe(
+  tap(debugOrderEvents),
   share(),
 )
 
 const price$ = priceTmp.pipe(
   distinctUntilChanged((n, o) => n.bid === o.bid && n.ask === o.ask),
-  share(),
+  tap(debugPrice),
 )
 
 const position$ = positionTmp.pipe(
   distinctUntilChanged((n, o) => n.price === o.price && n.qua === o.qua),
-  share(),
+  tap(debugPositions),
 )
 
 const BITMEX_API_KEY = process.env.BITMEX_API_KEY
@@ -92,5 +103,5 @@ export default {
   price$,
   position$,
   orders$,
-  orderEvents$: orderEvents.pipe(share()),
+  orderEvents$,
 }
