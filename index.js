6b8cdd6096fcd622fd0d9925938071495cb4dff2
Andrey Vorobyov <vorandrew@gmail.com>
2019-03-22 17:27:50 +0200

    orders$ refactored - does not work because of official bitmex lib bug

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 import 'dotenv/config'
-
-import crypto from 'crypto'
-import got from 'got'
-import qs from 'querystring'
 import Debug from 'debug'
 
-import { Subject, from, concat, timer } from 'rxjs'
-import { distinctUntilChanged, share, tap, switchMap, skip } from 'rxjs/operators'
+import { Subject } from 'rxjs'
+import { distinctUntilChanged, share, tap } from 'rxjs/operators'
 
 import BitMEXClient from 'bitmex-realtime-api'
 
-const BITMEX_API_KEY = process.env.BITMEX_API_KEY
-const BITMEX_API_SECRET = process.env.BITMEX_API_SECRET
+const INACTIVE_ORDERS = [
+  'Canceled',
+  'Done for day',
+  'Expired',
+  'Filled',
+  'Pending Cancel',
+  'Rejected',
+  'Replaced',
+  'Stopped',
+  'Suspended',
+]
 
 const client = new BitMEXClient({
   apiKeyID: process.env.BITMEX_API_KEY || null,
   apiKeySecret: process.env.BITMEX_API_SECRET || null,
 })
 
-const gotClient = got.extend({
-  baseUrl: 'https://www.bitmex.com',
-  headers: {
-    'content-type': 'application/json',
-    Accept: 'application/json',
-    'api-key': BITMEX_API_KEY,
-  },
-})
-
 const debugOrders = Debug('bitmex-rxjs:orders')
-const debugOrderEvents = Debug('bitmex-rxjs:order_events')
 const debugPosition = Debug('bitmex-rxjs:position')
 const debugPrice = Debug('bitmex-rxjs:price')
 
@@ -51,15 +46,9 @@ client.addStream('XBTUSD', 'position', data => {
   positionTmp.next({ price, qua })
 })
 
-export const orders$ = concat(timer(0), orderEvents).pipe(
-  switchMap(() => from(ordersPromise())),
-  tap(debugOrders),
-)
+// https://github.com/BitMEX/api-connectors/issues/294
 
-export const orderEvents$ = orderEvents.pipe(
-  tap(debugOrderEvents),
-  share(),
-)
+export const orders$ = orderEvents.pipe(tap(debugOrders))
 
 export const price$ = priceTmp.pipe(
   distinctUntilChanged((n, o) => n.bid === o.bid && n.ask === o.ask),
@@ -74,30 +63,3 @@ export const position$ = positionTmp.pipe(
   tap(debugPosition),
   share(),
 )
-
-function ordersPromise() {
-  const verb = 'GET',
-    path = '/api/v1/order?' + qs.stringify({ filter: JSON.stringify({ open: 'true' }) }),
-    expires = Math.round(new Date().getTime() / 1000) + 60
-
-  const signature = crypto
-    .createHmac('sha256', BITMEX_API_SECRET)
-    .update(verb + path + expires)
-    .digest('hex')
-
-  const requestOptions = {
-    headers: {
-      'api-expires': expires,
-      'api-signature': signature,
-    },
-    responseType: 'json',
-  }
-
-  return gotClient
-    .get(path, requestOptions)
-    .then(r => JSON.parse(r.body))
-    .catch(error => {
-      const errJSON = JSON.parse(error.response.body)
-      throw Error(errJSON.error.message)
-    })
-}
